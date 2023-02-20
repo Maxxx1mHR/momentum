c03b8e4ee94a5eed56b0ba4d36cb6bb80425ba46
Your Name <you@example.com>
2023-02-20 20:26:59 +0300

    feat: add greeting translation from ru/en, with using local storage.

--- momentum/src/js/modules/greeting.js
+++ momentum/src/js/modules/greeting.js
@@ -1,8 +1,13 @@
 //Greeting
 const greeting = document.querySelector('.greeting');
 const userName = document.querySelector('.name');
+const translateButtonRu = document.querySelector('.translate__btn_ru');
+const translateButtonEn = document.querySelector('.translate__btn_en');
 
-
+const greetingTranslation = {
+    ru: ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Спокойной ночи'],
+    en: ['Good morning', 'Good afternoon', ' Good evening', 'Good night']
+}
 //Получение времени суток
 function getTimeOfDay() {
     const currentDate = new Date();
@@ -20,18 +25,62 @@ function getTimeOfDay() {
 
 
 //Вывод приветсвия
+// function showGreeting() { 
+//     const currentDate = new Date();
+//     const hours = currentDate.getHours();
+//     if (hours >= 6 && hours < 12) {
+//         greeting.textContent = `Доброе утро`;
+//     } else if (hours >= 12 && hours < 18) {
+//         greeting.textContent = `Добрый день`;
+//     } else if (hours >= 18 && hours < 24) {
+//         greeting.textContent = `Добрый вечер`;
+//     } else {
+//         greeting.textContent = 'Спокойной ночи';
+//     }
+// }
+
+translateButtonRu.addEventListener('click', () => {
+    localStorage.setItem('language', 'ru');
+    translateButtonEn.classList.remove('translate_active');
+    translateButtonRu.classList.add('translate_active');
+ });
+
+translateButtonEn.addEventListener('click', () => {
+   localStorage.setItem('language', 'en');
+   translateButtonEn.classList.add('translate_active');
+   translateButtonRu.classList.remove('translate_active');
+});
+
+
+
+
 function showGreeting() { 
     const currentDate = new Date();
     const hours = currentDate.getHours();
-    if (hours >= 6 && hours < 12) {
-        greeting.textContent = `Доброе утро`;
-    } else if (hours >= 12 && hours < 18) {
-        greeting.textContent = `Добрый день`;
-    } else if (hours >= 18 && hours < 24) {
-        greeting.textContent = `Добрый вечер`;
+
+    if (localStorage.getItem('language') === 'ru') {
+        if (hours >= 6 && hours < 12) {
+            greeting.textContent = greetingTranslation.ru[0];
+        } else if (hours >= 12 && hours < 18) {
+            greeting.textContent = greetingTranslation.ru[1];
+        } else if (hours >= 18 && hours < 24) {
+            greeting.textContent = greetingTranslation.ru[2];
+        } else {
+            greeting.textContent = greetingTranslation.ru[3];
+        }
     } else {
-        greeting.textContent = 'Спокойной ночи';
+        if (hours >= 6 && hours < 12) {
+            greeting.textContent = greetingTranslation.en[0];
+        } else if (hours >= 12 && hours < 18) {
+            greeting.textContent = greetingTranslation.en[1];
+        } else if (hours >= 18 && hours < 24) {
+            greeting.textContent = greetingTranslation.en[2];
+        } else {
+            greeting.textContent = greetingTranslation.en[3];
+        }
     }
+
+
 }
 
 //Установка имени пользователеем
